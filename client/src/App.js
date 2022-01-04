9b4cd5b284bf5a29b02f49c847bd0edd63b08baa
Hamzamed <hamzaa.mohamed1998@gmail.com>
2022-01-04 20:49:28 +0100

    Building the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import MovieList from "./components/MovieList";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>Watch List</h1>
+        <MovieList />
+      </div>
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/components/MovieList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function MovieList() {
+  return (
+    <div>
+      <ul>
+        <li>Movie List</li>
+      </ul>
+    </div>
+  )
+}
